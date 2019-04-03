df2a2d9a18499fbf164a3485261979879eb28b85
Nam Hoang Nguyen <namqhong@gmail.com>
2019-04-03 11:41:42 +0700

    initialize proj

---
+++ DiemViewerMain/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DiemViewerMain.Views;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace DiemViewerMain
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationTestView>("NavigationTestView");
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+    }
+}
---
+++ DiemViewerMain/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiemViewerMain.Views;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace DiemViewerMain.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private IRegionManager _regionManager;
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+
+            _regionManager.RegisterViewWithRegion("MainContentRegion", typeof(RegisteredWithRegionTestView));
+
+            _regionManager.RequestNavigate("MainContentRegion", "NavigationTestView", result =>
+            {
+
+            });
+        }
+    }
+}
